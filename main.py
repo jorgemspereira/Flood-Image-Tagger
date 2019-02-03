d0f6b5746f0e0421e98baa9c59dcbd66fed6a2f7
jorgemspereira <jmspereira125@gmail.com>
2019-02-03 22:41:08 +0000

    Add arguments to read path to csv file and path to dataset.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+import argparse
 from tkinter import Tk
 
 from models.Window import Window
 
 
-def main():
-    path_to_csv = "C:\\Users\\jorge\\Desktop\\lol.csv"
-    path_to_dataset = "C:\\Users\\jorge\\Desktop\\Thesis\\datasets\\" \
-                      "MediaEval2017\\Classification\\development_set\\devset_images"
-
+def start_window(args):
     root = Tk()
     root.geometry("810x375")
     root.resizable(False, False)
-    app = Window(path_to_csv, path_to_dataset, root)
+    app = Window(args.path_to_csv, args.path_to_dataset, root)
     root.mainloop()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Flood image tagger.")
+    parser.add_argument("--images", dest="path_to_dataset", action="store_const", const=None,
+                        default="C:\\Users\\jorge\\Desktop\\Thesis\\datasets\\MediaEval2017"
+                                "\\Classification\\development_set\\devset_images",
+                        help="path for the MediaEval 2017 dataset")
+    parser.add_argument("--result", dest="path_to_csv", action="store_const",
+                        default="C:\\Users\\jorge\\Desktop\\lol.csv", const=None,
+                        help="path for the .csv file containing the classification")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    start_window(parse_args())
